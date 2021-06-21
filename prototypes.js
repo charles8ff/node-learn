8a684108c7b8ccf4fd64b28fa9c10388c021b390
Carlos Fisac Ferrández <carlos.fisac@bosonit.com>
2021-06-21 17:14:34 +0200

    heritance and such

--- prototypes.js
+++ prototypes.js
@@ -12,3 +12,17 @@ Person.prototype.wave = function() {
 }
 
 man.wave();
+
+function Spy(name){
+    Person.call(this, name);
+    //this calls the person's constructor with spy's this. So it gets the property and the parameter
+}
+
+Spy.prototype = new Person('i am a prototype');
+
+let daSpy = new Spy('Wilson')
+daSpy.wave();
+console.log(Object.getPrototypeOf(daSpy))
+console.log( daSpy instanceof Spy)
+console.log( daSpy instanceof Person)
+console.log( daSpy instanceof Object)
